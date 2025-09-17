8e72456ed80f5dc693039c292d6bdd3a290e58a8
Agden <chouw59@hotmail.fr>
2025-09-17 17:30:30 +0200

    chore monorepo front+back (ecovoit-front / ecovoit-back)

--- ecovoit-back/src/server.js
+++ ecovoit-back/src/server.js
@@ -13,3 +13,40 @@ app.use('/api', authRoutes);
 app.listen(PORT, () => {
   console.log(`EcoVoit’ API démarrée sur http://localhost:${PORT}`);
 });
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail", // si tu utilises Gmail
+  auth: {
+    user: "ecovoit.test@gmail.com",
+    pass: "yfptmbjhsqfeinrx"
+  }
+});
+
+app.post("/contact", (req, res) => {
+  const { nom, email, motif, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: "ecovoit.test@mail.com",
+    subject: `Contact : ${motif} - ${nom}`,
+    text: `
+Nom : ${nom}
+Email : ${email}
+Motif : ${motif}
+Message :
+${message}
+    `
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Erreur :", error);
+      res.status(500).send("Une erreur est survenue lors de l'envoi.");
+    } else {
+      console.log("Email envoyé :", info.response);
+      res.send("Merci ! Votre message a bien été envoyé.");
+    }
+  });
+});
